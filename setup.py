942c923708e936de68d13ae364b2b5d99b28800b
Paul Barton <pablo.barton@gmail.com>
2015-05-18 14:01:13 -0400

    setting up the setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import os
+
+def long_desc():
+    readme = os.path.join(os.path.dirname(__file__), 'README.md')
+    with open(readme, 'r') as inpt:
+        readme_text = inpt.read()
+    return readme_text
+
+setup(name='gruepy',
+      version='0.0.1'
+      long_description=long_desc(),
+      author='Paul Barton',
+      author_email='pablo.barton@gmail.com',
+      url='https://github.com/SavinaRoja/gruepy',
+      packages=['gruepy',],
+      license='http://www.gnu.org/licenses/gpl-3.0.html',
+      keywords='gruepy, curses, ncurses, terminal, console, UI, asynchronous',
+      )
+
